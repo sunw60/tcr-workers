71661d551b6f0e80046b5ea6856daa945fedd6ab
William Sun <sunw60@gmail.com>
2021-11-06 16:23:23 -0400

    linked doctor search button to data analysis apge

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import vlogo from './Vlogo.png';
+import { 
+    Route, Routes, Router, Link
+ } from 'react-router-dom'
+import Path3 from './Path3.js'
 export default function Home() {
     return (
         <div>
@@ -8,8 +12,14 @@ export default function Home() {
                 <img src={vlogo} className="App-logo" alt="vlogo" />
                 </div>
                 <div>
+                    <Link to="/path3" exact>
                 <button class="button-77" role="button">Search Prescription Data</button>
+                </Link>
+                <Routes>
+                    <Route path="/path3" element={<Path3/>} />      
+                </Routes>
                 </div>
+            
         </body>
         </div>
     )
