37632a9afd6cd54a6ead92e4186791d432e46af4
Kyle Shin <kshinus@gmail.com>
2021-11-06 16:15:55 -0400

    Commit

--- flask-server/prescriberData.py
+++ flask-server/prescriberData.py
@@ -3,12 +3,12 @@ from sys import argv
 
 def func(arg):
     df = pd.read_csv(arg, sep=",")
-    cholecap = df[df.Product == "Cholecap"]
-    zap = df[df.Product == "Zap-a-Pain"]
-    trx = cholecap.loc[:,cholecap.columns.str.startswith("TRx")]
-    names = cholecap.loc[:,cholecap.columns.str.endswith("name")]
-    result = pd.concat([names, trx], axis=1)
-    result = result.to_json()
+    #cholecap = df[df.Product == "Cholecap"]
+    #zap = df[df.Product == "Zap-a-Pain"]
+    #trx = cholecap.loc[:,cholecap.columns.str.startswith("TRx")]
+    #names = cholecap.loc[:,cholecap.columns.str.endswith("name")]
+    #result = pd.concat([names, trx], axis=1)
+    result = df.to_json()
     #print(df["Product"].unique())
     return (result)
 
